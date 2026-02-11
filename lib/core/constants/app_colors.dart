d68b4715ba479ea2e9fceac40b2f3d97ca6e2a89
ыыы <yyy@yyys-MacBook-Pro.local>
2026-02-11 17:38:28 +0500

    iconlar yigildi

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -6,10 +6,40 @@ class AppColors {
   static const Color primary = Color(0xFF0077FF);
   static const Color secondary = Color(0xFFFFC107);
 
-  static const Color background = Color(0xFFF5F5F5);
+
   static const Color surface = Colors.white;
 
   static const Color textPrimary = Color(0xFF1A1A1A);
   static const Color textSecondary = Color(0xFF757575);
+  static const Color blue500 = Color(0xFF1262D1);
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color cardBorderColor = Color(0xFFDCDDDE);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black500 = Color(0xFF171D23);
+  static const Color black50 = Color(0xFFF2F2F2);
+  static const Color blue600 = Color(0xFF044FB9);
+  static const Color black400 = Color(0xFF334150);
+  static const Color black300 = Color(0xFF798898);
+
+  static const Color textYellow500 = Color(0xFFEAA121);
+  static const Color restangleColor = Color(0xFF36B3E4);
+  static const Color divider = Color(0xFF334150);
+  static const Color blue50 = Color(0xFFE7EFFA);
+  static const Color black100 = Color(0xFFDCDDDE);
+  static const Color red = Color(0xFFED3023);
+  static const Color red50 = Color(0xFFFDEAE9);
+  static const Color green = Color(0xFF108737);
+  static const Color orange = Color(0xFFFE7226);
+  static const Color black200 = Color(0xFFB0B5BB);
+  static const Color black700 = Color(0xFF070A0C);
+  static const Color black600 = Color(0xFF101419);
+  //static const Color textSecondary = Color(0xFF757575);
+  //static const Color textSecondary = Color(0xFF757575);
+  //static const Color textSecondary = Color(0xFF757575);
+  //static const Color textSecondary = Color(0xFF757575);
+  //static const Color textSecondary = Color(0xFF757575);
+  //static const Color textSecondary = Color(0xFF757575);
+  //static const Color textSecondary = Color(0xFF757575);
+  //static const Color textSecondary = Color(0xFF757575);
 }
 
